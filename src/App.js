9ba8f3ea25a3a5f91567292d556d0286469c21a7
Marvin Scheffold <m@marvinscheffold.com>
2023-06-20 18:23:10 +0200

    bug reproduction

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import WebViewer from "@pdftron/webviewer";
+import "./App.css";
 
 const App = () => {
-  const viewer = useRef(null);
+    const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
-  useEffect(() => {
-    WebViewer(
-      {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-        licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
-      },
-      viewer.current,
-    ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+    // if using a class, equivalent of componentDidMount
+    useEffect(() => {
+        WebViewer(
+            {
+                path: "/webviewer/lib",
+                licenseKey:
+                    "demo:1687163586420:7d9c607f03000000009d77a8d8d8163d19b0629fa2cd6d05536e45fdbf",
+                enableOfficeEditing: true,
+                enableFilePicker: true,
+            },
+            viewer.current
+        ).then((instance) => {
+            const { documentViewer, annotationManager, Annotations } =
+                instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
+            instance.UI.setHeaderItems((header) => {
+                header.push({
+                    type: "actionButton",
+                    img: "...",
+                    onClick: async () => {
+                        const doc = documentViewer.getDocument();
+                        const data = await doc.getFileData({
+                            downloadType: "office",
+                        });
+                        const arr = new Uint8Array(data);
+                        const blob = new Blob([arr], {
+                            type: "application/docx",
+                        });
+
+                        // Add code for handling Blob here
+                        const url = URL.createObjectURL(blob);
+                        window.open(url);
+                    },
+                });
+            });
         });
+    }, []);
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">React sample</div>
+            <div className="webviewer" ref={viewer}></div>
+        </div>
+    );
 };
 
 export default App;
